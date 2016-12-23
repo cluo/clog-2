943c0c60ed7451080ff6d081dd92557ba76d1f0b
raylu <lurayl@gmail.com>
2016-12-23 00:08:21 -0600

    templating for logs

--- web.go
+++ web.go
@@ -4,6 +4,7 @@ import (
 	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -23,6 +24,7 @@ func serve(addr string) {
 	}
 
 	http.Handle("/", http.HandlerFunc(webIndex))
+	http.Handle("/style.css", http.HandlerFunc(webCss))
 	http.Handle("/log/", http.HandlerFunc(webLog))
 	http.Handle("/search", http.HandlerFunc(webSearch))
 	err = http.ListenAndServe(addr, nil)
@@ -40,11 +42,26 @@ func webIndex(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func webCss(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	http.ServeFile(w, req, "style.css")
+}
+
 func webLog(w http.ResponseWriter, req *http.Request) {
-	const logRoot = "/home/raylu/irclogs"
+	src, err := ioutil.ReadFile("log.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	tpl, err := template.New("log").Parse(string(src))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
 	relpath := req.URL.Path[5:] // strip "/log/"
 	relpath = path.Clean(relpath)
-	filepath := fmt.Sprintf("%s/%s.gz", logRoot, relpath)
+	filepath := fmt.Sprintf("%s/%s.gz", config.Irclogs, relpath)
 
 	var reader io.ReadCloser
 	rawReader, err := os.Open(filepath)
@@ -55,7 +72,7 @@ func webLog(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		// try again without .gz
-		reader, err = os.Open(fmt.Sprintf("%s/%s", logRoot, relpath))
+		reader, err = os.Open(fmt.Sprintf("%s/%s", config.Irclogs, relpath))
 		if err != nil {
 			http.Error(w, fmt.Sprintf("couldn't find \"%s\"", req.URL.Path[5:]), 404)
 			return
@@ -69,8 +86,21 @@ func webLog(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 	defer reader.Close()
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	_, err = io.Copy(w, reader)
+	logBytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	context := struct {
+		Relpath string
+		Log     string
+	}{
+		Relpath: relpath,
+		Log:     string(logBytes),
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = tpl.Execute(w, context)
 	if err != nil {
 		fmt.Println(err)
 	}
