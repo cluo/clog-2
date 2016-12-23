f0b7f700f766daa777f16f2196719b5dcb3a4862
raylu <lurayl@gmail.com>
2016-12-23 12:08:55 -0600

    scroll to highlight line

--- web/log.js
+++ web/log.js
@@ -1,3 +1,11 @@
 (() => {
 	'use strict';
+	if (document.location.hash <= 1)
+		return;
+
+	var el = document.querySelector(document.location.hash);
+	el.classList.add('highlight');
+	setTimeout(() => {
+		scrollTo(0, el.offsetTop - window.innerHeight / 2);
+	}, 0);
 })();
